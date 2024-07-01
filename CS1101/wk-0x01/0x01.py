e2ef5aff204beb2dbf1b843a5c1b9d3a893d11ef
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-01 16:38:41 +0100

    Feat: Finished first task

---
+++ CS1101/wk-0x01/0x01.py
@@ -0,0 +1,17 @@
+"""
+    Define a function that takes an argument. Call the function. 
+    Identify what code is the argument and what code is the parameter.
+"""
+
+
+
+def introduce(name):
+    # The name is the parameter which is a placeholder for the actual value
+    print(f"Hello, my name is {name}. I'm schooling at UoPeople.")
+
+
+
+
+
+# Adedoyin Emmanuel is the argument 
+introduce("Adedoyin Emmanuel") 
