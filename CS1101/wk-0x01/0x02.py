0dc8434ab6acefaf5731ecfd9c214a4daa37a42a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-01 17:31:18 +0100

    Feat: Added 3rd task

---
+++ CS1101/wk-0x01/0x02.py
@@ -0,0 +1,15 @@
+"""
+    Construct a function with a local variable. 
+    Show what happens when you try to use that variable outside the function.
+    Explain the results.
+"""
+
+
+def greet():
+    # Create a local variable
+    name = "Adedoyin Emmanuel" 
+
+
+
+# Trying to use the local variable
+print(name)
