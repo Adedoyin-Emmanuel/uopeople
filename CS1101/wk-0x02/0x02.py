df577f113f0c1d8d8c32e2051a55b32a0a7a3f62
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-06 07:58:53 +0100

    feat: finished second task

--- CS1101/wk-0x02/0x02.py
+++ CS1101/wk-0x02/0x02.py
@@ -1,3 +1,30 @@
 """
-    
+    The following is the countdown function copied from Section 5.8 of your textbook. 
+ 
+    def countdown(n): 
+        if n <= 0: 
+            print('Blastoff!') 
+        else: 
+            print(n) 
+            countdown(n-1) 
+
+    Write a new recursive function countup that expects a negative argument and counts “up” from that number. Output from running the function should look something like this: 
+
+    >>> countup(-3) 
+    -3 
+    -2 
+    -1 
+    Blastoff! 
 """
+
+
+
+def counttup(n):
+    if(n  >= 0 ):
+        print("Blastoff!")
+    else:
+        print(n)
+        counttup(n  + 1)
+
+
+counttup(-10)
