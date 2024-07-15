e7b17e1d536f57aa78e871a71cf306dfc37ab3a3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-15 17:11:44 +0100

    feat: finished fourth task

---
+++ CS1101/wk-0x03/0x04.py
@@ -0,0 +1,11 @@
+import math
+
+def hypotenuse(a, b):
+    return math.sqrt(a**2 + b**2)
+
+
+
+# Test cases
+print(hypotenuse(3, 4))  # Expected output: 5.0
+print(hypotenuse(5, 12)) # Expected output: 13.0
+print(hypotenuse(8, 15)) # Expected output: 17.0
