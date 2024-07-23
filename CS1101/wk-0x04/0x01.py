fe0004226b06a82495f852cf3d513644392665a5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-23 06:07:49 +0100

    feat: finished discussion forum tasks

--- CS1101/wk-0x04/0x01.py
+++ CS1101/wk-0x04/0x01.py
@@ -32,3 +32,57 @@ def any_lowercase2(s):
 print(any_lowercase2("abc"))  # Output: True
 print(any_lowercase2("ABC"))  # Output: True
 print(any_lowercase2("123"))  # Output: True
+
+
+
+"""
+    Function 3
+"""
+
+def any_lowercase3(s):
+    for c in s:
+        flag = c.islower()
+    return flag
+
+
+print(any_lowercase3("abc"))  # Output: True
+print(any_lowercase3("ABC"))  # Output: False
+print(any_lowercase3("Abc"))  # Output: True
+
+
+
+"""
+    Function 4
+"""
+
+def any_lowercase4(s):
+    flag = False
+    for c in s:
+        flag = flag or c.islower()
+    return flag
+
+
+print(any_lowercase4("abc"))  # Output: True
+print(any_lowercase4("ABC"))  # Output: False
+print(any_lowercase4("Abc"))  # Output: True
+
+
+
+
+"""
+    Function 5
+"""
+
+
+def any_lowercase5(s):
+    for c in s:
+        if not c.islower():
+            return False
+    return True
+
+
+
+
+print(any_lowercase5("abc"))  # Output: True
+print(any_lowercase5("ABC"))  # Output: False
+print(any_lowercase5("Abc"))  # Output: False
