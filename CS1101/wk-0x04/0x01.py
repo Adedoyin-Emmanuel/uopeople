6007b2437ad9060438cda65bdcf91216f603a90b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-23 05:50:55 +0100

    feat: finished second task

--- CS1101/wk-0x04/0x01.py
+++ CS1101/wk-0x04/0x01.py
@@ -1,3 +1,7 @@
+
+"""
+    Function 1
+"""
 def any_lowercase1(s):
     for c in s:
         if c.islower():
@@ -11,3 +15,20 @@ print(any_lowercase1("Abc"))  # Output: False
 print(any_lowercase1("aBc"))  # Output: True
 
 
+
+
+"""
+    Function 2
+"""
+def any_lowercase2(s):
+    for c in s:
+        if 'c'.islower():
+            return 'True'
+        else:
+            return 'False'
+
+
+
+print(any_lowercase2("abc"))  # Output: True
+print(any_lowercase2("ABC"))  # Output: True
+print(any_lowercase2("123"))  # Output: True
